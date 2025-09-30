9826b08597638976c72576bdeb02b54f13ce21b0
JUAN FRANCISCO RIVAS RODRIGO <jrivarod@myuax.com>
2025-09-30 14:28:01 +0200

    Delete Ficheros2025/Ficheros2025 directory

--- Ficheros2025/Ficheros2025/src/controlador/NumeroInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package controlador;
-
-public class NumeroInvalidoException extends RuntimeException {
-    public NumeroInvalidoException(String message) {
-        super("Inserte un valor numérico. " + message);
-    }
-}
